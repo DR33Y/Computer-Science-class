3ebfb183498fd30ee5103c5bc4631eb64ea999da
DR33Y <74345638+DR33Y@users.noreply.github.com>
2023-10-23 11:22:43 +0300

    Add files via upload

---
+++ Sukkot Mission.java
@@ -0,0 +1,124 @@
+package Study;
+
+class Main {
+	public static void main(String[] args) {
+		int x=4, y=10, z=90;
+		System.out.println(x+"\n"+y+"\n"+z);
+	}
+}
+
+
+class Main {
+	public static viod main(Strung[] args) {
+		int x=4;
+		float y=5.9f;
+		System.out.println(x+"   "+y);
+	}
+}
+
+
+class Main {
+	public static void main(String[] args) {
+		char c1='D';
+		char c2='a';
+		char c3='n';
+		char c4='i';
+		char c5='e';
+		char c6='l';
+		System.out.println(c1+""+c2+""+c3+""+c4+""+c5+""+c6);
+	}
+}
+
+
+class Main {
+	public static void main(String[] args) {
+		int num1=23, num2=58;
+		System.out.println(num1+" is less then "+num2);
+	}
+}
+	
+
+class Main {
+	public static void main(String[] args) {
+		int num1=3, num2=6, num3=9;
+		float numAve=(float)(num1+num2+num3)/3;
+		System.out.println("The numbers average is "+numAve);
+	}
+}
+	
+
+class Main {
+	public static void main(String[] args) {
+		int a=3, b=4;
+		int c=(int) Math.pow(a,2) + (int) Math.pow(b,2);
+		System.out.println("C power 2 equal")
+	}
+}
+
+
+class Main {
+	public static void main(String[] args) {
+		int x=4;
+		int y = (2*x + 5);
+		System.out.println("The equation y=2x+5 is equal to. y="+y);
+	}
+}
+
+
+class Main {
+	public static void main(String[] args) {
+		String str1="I love to study computer science";
+		String str2=" and therfore redy to invest";
+		String str3=" hours a day for practice";
+		int hour = 10;
+		System.out.println(str1+str2+" "+hour+str3);
+	}
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
