d8911c0ca99888b2555486fb931dc766b6074928
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-01-16 15:19:38 +0200

    Update lesson 5.java

--- lesson 5.java
+++ lesson 5.java
@@ -133,3 +133,105 @@ public class Tester {
 		System.out.println("The main value is "+f8(a1,n,d));
 	}
 }
+
+/תרגיל 9
+import java.util.Scanner; 
+
+public class Tester {
+	public static String f9(String str) {
+		Double str_len = Math.pow(str.length(),2);
+		System.out.println(str_len);
+		return str;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sc.next();
+		f9(str);
+	}
+}
+
+/תרגיל 10
+import java.util.Scanner; 
+
+public class Tester {
+	public static String f10(String str1, String str2) {
+		if (str1.length() > str2.length())
+			return str1;
+		else 
+			return str2;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 string");
+		String str1 = sc.next(); 
+		String str2 = sc.next(); 
+		String long_str = f10(str1, str2);
+		System.out.println(long_str);
+	}
+}
+
+/תרגיל 11
+import java.util.Scanner; 
+
+public class Tester {
+	public static boolean f11(String str, int num) {
+		int str_len = str.length();
+		if (str_len == num)
+			return true;
+		else
+			return false;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string and number");
+		String str = sc.next();
+		int num = sc.nextInt();
+		boolean ans = f11(str, num);
+		System.out.println(ans);
+	}
+}
+
+/תרגיל 12
+import java.util.Random; 
+
+public class Tester {
+	public static boolean f12(int num1, int num2) {
+		if (num1 == num2)
+			return true;
+		else
+			return false;
+	}
+	public static void main(String[] args) {
+		Random r = new Random();
+		int num1 = r.nextInt(6)+1;
+		int num2 = r.nextInt(6)+1;
+		System.out.println("number1:"+num1+" number2:"+num2);
+		boolean ans = f12(num1, num2);
+		System.out.println(ans);
+	}
+}
+
+/תרגיל 13
+import java.util.Scanner; 
+
+public class Tester {
+	public static boolean f13(char letter, String word){
+		int index = word.indexOf(letter);
+		if (index >= 0){
+			System.out.println("The char "+letter+" is in "+word);
+			return true;
+		}
+		else {
+			System.out.println("The char "+letter+" is in "+word);
+			return false;
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		String word = sc.next();
+		boolean ans = f13('d',word);
+		System.out.println(ans);
+	}
+}
