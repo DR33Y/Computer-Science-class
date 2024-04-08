02e3e6fd09d57d8c46f84d28ea167db6eaaf5035
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-04-08 23:42:23 +0300

    Update lesson7_A.java

--- lesson7_A.java
+++ lesson7_A.java
@@ -1,5 +1,5 @@
 //Point Class
-  public class Point {
+    public class Point {
   	private int _x;
   	private int _y;
   	public Point(int x, int y) {
@@ -63,4 +63,50 @@
 		
 	}
 }
+
+//Student
+   public class Student {
+	private String _name;
+	private String _lastName;
+	private int _id;
+	public Student(String name, String lastName, int id) {
+		_name = name;
+		_lastName = lastName;
+		_id = id;
+	}
+	
+	public String toString() {
+		return ("Student first name: "+_name+"\nStudent last name: "+_lastName+"\nStudent id: "+_id);
+	}
+    }
+
+    public class Tester {
+  	public static void main(String[] args) {
+  		Student s1 = new Student("Daniel","Ross",1234);
+  		System.out.println(s1);
+  	}
+    }  
+
+//Book
+    public class Book {
+	private String _name;
+	private String _author;
+	private int _categoryNumber;
+	public Book(String name, String author, int categoryNumber) {
+		_name = name;
+		_author = author;
+		_categoryNumber = categoryNumber; 
+	}
+	
+	public String toString() {
+		return ("Book name: "+_name+"\nAuthor name: "+_author+"\nBook category number: "+_categoryNumber);
+	}
+    }
+
+    public class Tester {
+  	public static void main(String[] args) {
+  		Book b1 = new Book("Daniel","Story",2);
+  		System.out.println(b1);
+  	}	
+    } 
 
