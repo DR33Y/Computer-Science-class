f89e8c8c7ae8db3d1f7caa5c0922da853fd8e0e2
DR33Y <74345638+DR33Y@users.noreply.github.com>
2024-05-07 15:53:46 +0300

    Update lesson12.java

--- lesson12.java
+++ lesson12.java
@@ -1,67 +1,71 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Tester {
 	public static void f1(int row, int col) {
 		Random r = new Random();
+		
 		int[][] arr = new int[row][col];
 		int i,j;
-		for(i=0; i < arr.length; i++) {
-			for(j=0; j < arr[i].length; j++) {
+		for(i=0; i<arr.length; i++) {
+			for(j=0; j<arr[i].length; j++)
 				arr[i][j] = r.nextInt(0,101);
-				System.out.print(arr[i][j]+" ");
-			}
-		System.out.println();
 		}
 		
+		for(i=0; i<arr.length; i++) {
+			for(j=0; j<arr[i].length; j++)
+				System.out.print(arr[i][j]+" ");
+			System.out.println();
+		}
 	}
 	
 	public static void f2() {
 		Scanner sc = new Scanner(System.in);
+		
 		int[][] arr = new int[3][3];
 		int i,j;
+		int max=0;
 		for(i=0; i<arr.length; i++) {
 			for(j=0; j<arr[i].length; j++) {
-				System.out.println("Enter row "+(i+1)+" and col "+(j+1)+": ");
+				System.out.println("Enter row "+i+", col "+j+":");
 				arr[i][j] = sc.nextInt();
 			}
 		}
-		for(i=0; i<arr.length; i++) {
-			for(j=0; j<arr[i].length; j++) {
+		
+		for(i=0; i<arr.length; i++) {   
+			for(j=0; j<arr[i].length; j++)
 				System.out.print(arr[i][j]+" ");
-			}
 			System.out.println();
 		}
+		
+		for(i=0; i<arr.length; i++) {
+			int sum=0;
+			for(j=0; j<arr[i].length; j++) {
+				sum += arr[i][j];
+			}
+			if(sum > max)
+				max = sum;
+		}
+		System.out.println("The biggest row sum is: "+max);
 	}
 	
 	public static void f3(int row, int col) {
 		Random r = new Random();
+		
 		int[][] arr1 = new int[row][col];
 		int[][] arr2 = new int[row][col];
-		int[][] arr3 = new int[row][col];
 		
 		int i,j;
-		//arr1 placement
-		for(i=0; i<arr1.length; i++) {
-			for(j=0; j<arr1[i].length; j++) {
+		for(i=0; i<row; i++) {
+			for(j=0; j<col; j++) {
 				arr1[i][j] = r.nextInt(0,101);
-				System.out.print(arr1[i][j]+" ");
+				arr2[i][j] = r.nextInt(0,101); 
 			}
-			System.out.println();
 		}
-		System.out.println();
-		//arr2 placement
-		for(i=0; i<arr2.length; i++) {
-			for(j=0; j<arr2[i].length; j++) { 
-				arr2[i][j] = r.nextInt(0,101);
-				System.out.print(arr2[i][j]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-		//arr3 placement
-		for(i=0; i<arr3.length; i++) {
-			for(j=0; j<arr3.length; j++) {
+		
+		int[][] arr3 = new int[row][col];
+		for(i=0; i<row; i++) {
+			for(j=0; j<col; j++) {
 				arr3[i][j] = arr1[i][j] + arr2[i][j];
 				System.out.print(arr3[i][j]+" ");
 			}
@@ -69,18 +73,42 @@ public class Tester {
 		}
 	}
 	
-	public static void f5(int[][] arr) {
+	public static void f4(int row, int col) {
+		Random r = new Random();
+		
+		int[][] arr = new int[row][col];
 		int i,j;
-		int sum=0;
-		for(i=0; i<arr.length; i++) {
-			for(j=0; j<arr[i].length; j++) {
-				if(j==i)
-					sum += arr[i][j];
-				System.out.print(arr[i][j]);
+		
+		System.out.println("Before change:");
+		for(i=0; i<row; i++) {
+			for(j=0; j<col; j++) {
+				arr[i][j] = r.nextInt(0,11);
+				System.out.print(arr[i][j]+" ");
 			}
 			System.out.println();
 		}
-		System.out.println(sum);
+		
+		System.out.println("After change:");
+		for(i=0; i<arr.length; i++) {
+			for(j=0; j<arr[i].length; j++) {
+				System.out.print(arr[j][i]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void f5(int[][] arr) {
+		int i;
+		int sum1=0, sum2=0;
+		
+		for(i=0; i<arr.length; i++) 
+			sum1 += arr[i][i];
+		System.out.println("diagonal 1 sum: "+sum1);
+		
+		for(i=0; i<arr.length; i++) {
+			sum2 += arr[(arr.length-1)-i][i];
+		}
+		System.out.println("diagonal 2 sum: "+sum2);
 	}
 	
 	public static boolean f6(int[][] arr) {
